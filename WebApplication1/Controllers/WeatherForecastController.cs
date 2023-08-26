b2648b080b4a015768de85aa284371b1a3bf4648
Leonardo Bezerra <leonardoab89@gmail.com>
2023-08-26 14:51:46 -0300

    Retirar log

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace WebApplication1.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        
 
         public WeatherForecastController()
         {
