fbe3ff0f779be290b40591b62d54827664450015
leonardo.bezerra <leonardo.bezerra@vpn>
2023-09-21 02:35:35 +0000

    Ajustes

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,7 @@
+using DockerInfnetDevOpsSample.HealthCheck;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +11,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+
+builder.Services.AddHealthChecks()
+                .AddSqlServer(
+                    connectionString: builder.Configuration.GetConnectionString("InfnetPosDb"),
+                    healthQuery: "SELECT 1",
+                    name: "Database",
+                    failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy
+                ).AddUrlGroup(new Uri("http://httpbin.org/status/200"), "Api Terceiro Nao Autenticada")
+                .AddCheck<HealthCheckRandom>(name: "Api Terceiro Autenticada"); ;
+               
+
+builder.Services.AddHealthChecksUI(s =>
+{
+    s.AddHealthCheckEndpoint("Infnet API", "https://localhost:44364/healthz");
+}).AddInMemoryStorage();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
@@ -21,5 +45,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseRouting()
+   .UseEndpoints(config =>
+   {
+       config.MapHealthChecks("/healthz", new HealthCheckOptions
+       {
+           Predicate = _ => true,
+           ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+       });
+
+       config.MapHealthChecksUI();
+   });
+
 app.Run();
- 
